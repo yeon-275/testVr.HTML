05f169eb0b824895424e4207d98f1b22ef4f231a
yeon-275 <52732322+yeon-275@users.noreply.github.com>
2019-09-17 20:49:22 +0900

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,23 @@
+document.querySelector("#1.1").addEventListener("click", (e)=>{
+console.log("Foo")
+})
+
+document.querySelector("#1.1.1").addEventListener("click", (e)=>{
+console.log("Foo")
+})
+
+document.querySelector("#1.1.2").addEventListener("click", (e)=>{
+console.log("Foo")
+})
+
+document.querySelector("#1.2").addEventListener("click", (e)=>{
+console.log("Foo")
+})
+
+document.querySelector("#1.2.1").addEventListener("click", (e)=>{
+console.log("Foo")
+})
+
+document.querySelector("#1.2.2").addEventListener("click", (e)=>{
+console.log("Foo")
+})
